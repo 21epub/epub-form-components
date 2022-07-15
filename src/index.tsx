55852aee8c40de74d7ce7801492088e842a9f341
dxsixpc <1071667708@qq.com>
2022-07-15 16:55:51 +0800

    feat: 导出布局组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import 'antd/dist/antd.css';
 // 通用表单组件
 export * from './FormComponents';
-// 定制化widget组件
-export * from './FormWidgets';
 // json配置面板
 export * from './FormJsonPanel';
+// 定制化widget组件
+export * from './FormWidgets';
+// 布局组件
+export * from './LayoutWidgets';
 // type类型
 export * from './type';
